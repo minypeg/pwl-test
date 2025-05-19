00ec9f64c4eab2e31a426a79ce35cb043eef4d12
Albeiro <albeiroespitiasierra@gmail.com>
2025-05-19 14:09:11 -0500

    feat: use chatopenai directly

--- llms/utils.py
+++ llms/utils.py
@@ -1,27 +1,15 @@
 """This module contains utility functions for LLM operations."""
 
 import os
-from langchain_openai import AzureChatOpenAI
+from langchain_openai import ChatOpenAI
 
 EAST_2_AZURE_ENDPOINT = os.environ.get("EAST_2_AZURE_ENDPOINT")
 EAST_2_AZURE_API_KEY = os.environ.get("EAST_2_AZURE_API_KEY")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
 
-def get_4o_east_2_llm():
-    return AzureChatOpenAI(
-        azure_endpoint=EAST_2_AZURE_ENDPOINT,
-        api_key=EAST_2_AZURE_API_KEY,
-        deployment_name="gpt-4o-2",
-        temperature=0,
-        api_version="2024-08-01-preview",
-    )
-
-
-def get_o3_mini_east_2_llm(**kwargs) -> AzureChatOpenAI:
-    return AzureChatOpenAI(
-        azure_endpoint=EAST_2_AZURE_ENDPOINT,
-        api_key=EAST_2_AZURE_API_KEY,
-        deployment_name="o3-mini",
-        api_version="2025-01-01-preview",
-        **kwargs
+def get_o3_mini_east_2_llm() -> ChatOpenAI:
+    return ChatOpenAI(
+        model="o3-mini",
+        disabled_params={"parallel_tool_calls": None},
     )
